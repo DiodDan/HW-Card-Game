151ed0a6a6a39fd4267617b3a232e68ef22b98e3
stoyanovvK <kirill.stoyanov2005k@gmail.com>
2024-07-10 21:05:04 +0200

    SoundEngine changes

--- src/SoundEngine/SoundEngine.java
+++ src/SoundEngine/SoundEngine.java
@@ -2,29 +2,29 @@ package SoundEngine;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+
 
 
 /** Function that creates sound */
 public class SoundEngine {
-
+    /** method to create a pullSound **/
     public void pullSound(float volume) {
         playSound("sounds/pullSound.wav", volume);
     }
-
+    /** method to create a winSound **/
     public void winSound(float volume) {
         playSound("sounds/winSound.wav", volume);
     }
-
+    /** method to create a gameSound **/
     public void gameSound(float volume) {
         playSound("sounds/background.wav", volume);
     }
-
+    /** method to create a shakeSound **/
     public void shakeSound(float volume) {
         playSound("sounds/shakeSound.wav", volume);
     }
 
+    /** method to play and control the volume **/
     private void playSound(String soundFilePath, float volume) {
         try {
             File soundFile = new File(soundFilePath);
